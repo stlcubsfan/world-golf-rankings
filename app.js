4d30ee061c4912170a6e922243df1ad31625c4c1
Brian Coles <stlcubsfan@gmail.com>
2016-08-11 20:25:48 -0500

    Enabled cors on options

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.options('*', cors());
 
 // development only
 if ('development' == app.get('env')) {
